dd406f0ebf22f397c2631cb22499d16124d6eb34
Ne_kolay <Yezzzhun@gmail.com>
2026-01-12 18:30:04 +0300

    configured database connection

---
+++ src/main/java/savchenko/dev/HibernateConfiguration.java
@@ -0,0 +1,24 @@
+package savchenko.dev;
+
+import org.hibernate.*;
+import org.springframework.context.annotation.*;
+import savchenko.dev.Model.*;
+
+@Configuration
+public class HibernateConfiguration {
+    @Bean
+    public SessionFactory sessionFactory() {
+        org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+        configuration
+                .addAnnotatedClass(Account.class)
+                .addAnnotatedClass(User.class)
+                .addPackage("savchenko.dev")
+                .setProperty("hibernate.connection.driver_class", "org.postgresql.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres")
+                .setProperty("hibernate.connection.username", "postgres")
+                .setProperty("hibernate.connection.password", "root")
+                .setProperty("hibernate.show_sql", "true")
+                .setProperty("hibernate.hbm2ddl.auto", "update");
+        return configuration.buildSessionFactory();
+    }
+}
